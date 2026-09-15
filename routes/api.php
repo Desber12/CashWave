976eec0903e879e5a1fdd1668aa4c0e1e5d73bc0
Yopa <admin@Yopa-MacBook-Air.local>
2026-09-15 15:48:49 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -3,32 +3,40 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ReportController;
 
 
+// User authenticated check
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// post login
-Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-// post logout  
-Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// api resource product
-Route::apiResource('products', \App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
-Route::get('/product/{id}', [\App\Http\Controllers\Api\ProductController::class, 'show']);
-Route::get('/product/category/{id}', [\App\Http\Controllers\Api\ProductController::class, 'filterByCategory']);
-Route::get('/products/category/{id}', [\App\Http\Controllers\Api\ProductController::class, 'getByCategory']);
-Route::get('/products/category/{category}', [\App\Http\Controllers\Api\ProductController::class, 'getByCategory']);
 
-// api resource order
-Route::apiResource('orders', \App\Http\Controllers\Api\OrderController::class)->middleware('auth:sanctum');
+// PRODUCT ROUTES
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product/category/{id}', [ProductController::class, 'filterByCategory']);
+Route::get('/products/category/{id}', [ProductController::class, 'getByCategory']);
+Route::get('/products/category/{category}', [ProductController::class, 'getByCategory']);
 
-// get categories
-Route::get('list-categories', [\App\Http\Controllers\Api\CategoryController::class, 'index'])->middleware('auth:sanctum');
+// API Resource Product
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
 
-// api resource report
-Route::get('/reports/summary', [App\Http\Controllers\Api\ReportController::class, 'summary'])->middleware('auth:sanctum');
-Route::get('/reports/product-sales', [App\Http\Controllers\Api\ReportController::class, 'productSales'])->middleware('auth:sanctum');
-Route::get('/reports/close-cashier', [App\Http\Controllers\Api\ReportController::class, 'closeCashier'])->middleware('auth:sanctum');
+// OTHER API ROUTES
+
+// Order resource
+Route::apiResource('orders', OrderController::class)->middleware('auth:sanctum');
+
+// Categories
+Route::get('list-categories', [CategoryController::class, 'index'])->middleware('auth:sanctum');
+
+// Reports
+Route::get('/reports/summary', [ReportController::class, 'summary'])->middleware('auth:sanctum');
+Route::get('/reports/product-sales', [ReportController::class, 'productSales'])->middleware('auth:sanctum');
+Route::get('/reports/close-cashier', [ReportController::class, 'closeCashier'])->middleware('auth:sanctum');
